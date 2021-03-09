8cb784562888d2735f27c740e97452239630e397
yn-s17 <yn-s17@DESKTOP-2U5R74I>
2021-03-09 14:07:59 +0900

    2021-03-09 14:07 쓸데없는 파일 삭제, Bean파일 추가

---
+++ src/vo/EDCBean.java
@@ -0,0 +1,61 @@
+package vo;
+//교육과정
+public class EDCBean {
+	private int EDC_Num; // '교육과정_번호',
+	private String EDC_Subject; // '교육과정_과목명',
+	private String EDC_Place;//'교육과정_장소',
+	private String EDC_Time;// '교육과정_시간',
+	private String EDC_PD; //'교육과정_기간',
+	private String EDC_NMPR; // '교육과정_모집인원',
+	private String EDC_Photo; //'교육과정_사진',
+	private String EDC_Content;// '교육과정_내용',
+	public int getEDC_Num() {
+		return EDC_Num;
+	}
+	public void setEDC_Num(int eDC_Num) {
+		EDC_Num = eDC_Num;
+	}
+	public String getEDC_Subject() {
+		return EDC_Subject;
+	}
+	public void setEDC_Subject(String eDC_Subject) {
+		EDC_Subject = eDC_Subject;
+	}
+	public String getEDC_Place() {
+		return EDC_Place;
+	}
+	public void setEDC_Place(String eDC_Place) {
+		EDC_Place = eDC_Place;
+	}
+	public String getEDC_Time() {
+		return EDC_Time;
+	}
+	public void setEDC_Time(String eDC_Time) {
+		EDC_Time = eDC_Time;
+	}
+	public String getEDC_PD() {
+		return EDC_PD;
+	}
+	public void setEDC_PD(String eDC_PD) {
+		EDC_PD = eDC_PD;
+	}
+	public String getEDC_NMPR() {
+		return EDC_NMPR;
+	}
+	public void setEDC_NMPR(String eDC_NMPR) {
+		EDC_NMPR = eDC_NMPR;
+	}
+	public String getEDC_Photo() {
+		return EDC_Photo;
+	}
+	public void setEDC_Photo(String eDC_Photo) {
+		EDC_Photo = eDC_Photo;
+	}
+	public String getEDC_Content() {
+		return EDC_Content;
+	}
+	public void setEDC_Content(String eDC_Content) {
+		EDC_Content = eDC_Content;
+	}
+	
+}
