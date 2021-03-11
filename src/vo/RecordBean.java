03f86d64d86a86c58d485aaa436da0df6a4bdd45
byeongik11 <mice2008@naver.com>
2021-03-11 10:13:57 +0900

    recodeBean vo 수정

--- src/vo/RecordBean.java
+++ src/vo/RecordBean.java
@@ -3,7 +3,7 @@ package vo;
 public class RecordBean {
 	private int record_Num;//'이력번호',
 //	private String teacher_ID;//'강사_아이디',
-	private int eDC_Num;//'교육과정_번호',
+	private int edc_Num;//'교육과정_번호',
 	
 	public int getRecord_Num() {
 		return record_Num;
@@ -11,12 +11,13 @@ public class RecordBean {
 	public void setRecord_Num(int record_Num) {
 		this.record_Num = record_Num;
 	}
-	public int geteDC_Num() {
-		return eDC_Num;
+	public int getEdc_Num() {
+		return edc_Num;
 	}
-	public void seteDC_Num(int eDC_Num) {
-		this.eDC_Num = eDC_Num;
+	public void setEdc_Num(int edc_Num) {
+		this.edc_Num = edc_Num;
 	}
+	
 	
 	
 	
